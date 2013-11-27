5010e07eb90843b1fda9baa51b3eca1344efc756
dazz <annejulia@gmail.com>
2013-11-27 15:33:33 +0100

    moved config from dist file to use environment variables to work with easybib deployments

--- tests/Easybib/Tests/Acceptance/oauthConfig.php
+++ tests/Easybib/Tests/Acceptance/oauthConfig.php
@@ -1,7 +1,10 @@
 <?php
 
 return [
-    'server.auth'   => 'http://data.easybib.com/oauth/authorize',
-    'client.id'     => 'test',
-    'client.secret' => 'test',
+    'server.auth'     => getenv('DISCOVER_API_OAUTH_URL') . '/oauth/authorize',
+    'server.token'    => getenv('DISCOVER_API_OAUTH_URL') . '/oauth/token',
+    'entrypoint.data' => getenv('DISCOVER_API_OAUTH_URL') . '/projects/',
+    'entrypoint.user' => getenv('DISCOVER_API_OAUTH_URL') . '/user/',
+    'client.id'       => getenv('DISCOVER_API_OAUTH_ID'),
+    'client.secret'   => getenv('DISCOVER_API_OAUTH_SESCRET'),
 ];
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,4 +3,9 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = require dirname(__DIR__) . '/src/app.php';
 
+putenv('DISCOVER_API_OAUTH_URL=http://localhost');
+putenv('DISCOVER_API_OAUTH_ID=test');
+putenv('DISCOVER_API_OAUTH_SECRET=test');
+
+
 return $app;
