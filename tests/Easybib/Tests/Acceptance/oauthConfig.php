bf52465546cd8d8e9129fa030144219c49f6236b
till <till@php.net>
2013-11-27 17:21:36 +0100

    Change: shorten environment variables

--- tests/Easybib/Tests/Acceptance/oauthConfig.php
+++ tests/Easybib/Tests/Acceptance/oauthConfig.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'server.auth'     => getenv('DISCOVER_API_OAUTH_URL') . '/oauth/authorize',
-    'server.token'    => getenv('DISCOVER_API_OAUTH_URL') . '/oauth/token',
-    'entrypoint.data' => getenv('DISCOVER_API_OAUTH_URL') . '/projects/',
-    'entrypoint.user' => getenv('DISCOVER_API_OAUTH_URL') . '/user/',
-    'client.id'       => getenv('DISCOVER_API_OAUTH_ID'),
-    'client.secret'   => getenv('DISCOVER_API_OAUTH_SESCRET'),
+    'server.auth'     => getenv('OAUTH_URL') . '/oauth/authorize',
+    'server.token'    => getenv('OAUTH_URL') . '/oauth/token',
+    'entrypoint.data' => getenv('OAUTH_URL') . '/projects/',
+    'entrypoint.user' => getenv('OAUTH_URL') . '/user/',
+    'client.id'       => getenv('OAUTH_ID'),
+    'client.secret'   => getenv('OAUTH_SESCRET'),
 ];
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,9 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 $app = require dirname(__DIR__) . '/src/app.php';
 
-putenv('DISCOVER_API_OAUTH_URL=http://localhost');
-putenv('DISCOVER_API_OAUTH_ID=test');
-putenv('DISCOVER_API_OAUTH_SECRET=test');
+putenv('OAUTH_URL=http://localhost');
+putenv('OAUTH_ID=test');
+putenv('OAUTH_SECRET=test');
 
 
 return $app;
