e9d64c81487bf8dd2a1a8c120ef921a45925a599
Joel Gilley <gilleyj@gmail.com>
2017-04-11 13:50:18 -0400

    - make travis build script and prep files for this change - relocate build_config.php and update post_boot.sh - remove build from dockerfile

--- docker/post_boot.sh
+++ docker/post_boot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build the configuration required from ENV vars post boot - pre start
-php /webroot/build_config.php
+php /webroot/docker/build_config.php
 
 # if [ "$APP_ENVIRONMENT" == "DEVELOPMENT" ] ; then
 	# This is here for debug purposes and is not required in production
